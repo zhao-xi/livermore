b269520b8b4bfa884aecbf1465da6cc3c8256a2d
zhaoxi0418 <zhaoxi0418@outlook.com>
2024-06-18 21:15:41 +0800

    empty funcs

--- market_data.py
+++ market_data.py
@@ -3,13 +3,11 @@ import okx.MarketData as MarketData
 
 flag = "0"  # 实盘:0 , 模拟盘：1
 marketDataAPI = MarketData.MarketAPI(flag=flag)
-day_highest = {}
-day_lowest = {}
 
-def get_date_and_close_price(start_ts, end_ts, bar="1D"):
+def get_candlesticks(start_ts, end_ts, bar="1D"):
     # 获取指数K线数据，整理为日期、收盘价格式
     finished = False
-    datetime_and_close_price = []  # 日期、收盘价
+    candlesticks = []  # 日期、收盘价
     while not finished:
         result = marketDataAPI.get_history_candlesticks(
             instId="BTC-USDT",
@@ -18,12 +16,37 @@ def get_date_and_close_price(start_ts, end_ts, bar="1D"):
             before=start_ts
         )
         for candle in result.get("data"):
-            datetime_and_close_price.append([convert_milliseconds_to_date_string(candle[0]), candle[4]])
-            day_highest[convert_milliseconds_to_date_string(candle[0])] = float(candle[2])
-            day_lowest[convert_milliseconds_to_date_string(candle[0])] = float(candle[3])
+            candlesticks.append([convert_milliseconds_to_date_string(candle[0]), candle[4]])
+            candlesticks.append({
+                "time": convert_milliseconds_to_date_string(candle[0]),
+                "open": candle[1],
+                "highest": candle[2],
+                "lowest": candle[3],
+                "close": candle[4]
+            })
         if len(result.get("data")) > 0 and float(result.get("data")[-1][0]) > start_ts:
             end_ts = int(result.get("data")[-1][0])
         else:
             finished = True
-    datetime_and_close_price = datetime_and_close_price[::-1]
-    return datetime_and_close_price
+    candlesticks = candlesticks[::-1]
+    return candlesticks
+
+
+def cal_max(candlesticks, period):
+    # 计算period期间最高价格
+    pass
+
+
+def cal_min(candlesticks, period):
+    # 计算period期间最低价格
+    pass
+
+
+def cal_avg(candlesticks, period):
+    # 计算period期间MA均线
+    pass
+
+
+def cal_avg_exp(candlesticks, period):
+    # 计算period期间EMA均线
+    pass
